5fae1812f23a550e68a6e9f5dcd454b4fc1b5f52
ivn <ivanderseah08@gmail.com>
2025-09-24 11:01:18 +0700

    read and clear null values

---
+++ powerlifting_regression.m
@@ -0,0 +1,12 @@
+clear; clc;
+
+data = readtable('openpowerlifting-2024-01-06-4c732975.csv');
+
+vars = {'Sex','Equipment','Age','BodyweightKg','Country',...
+        'Best3SquatKg','Best3BenchKg','Best3DeadliftKg'};
+
+data = data(:, vars);
+
+data = rmmissing(data);
+
+disp(head(data));
