2dc2ea5e24ffd704de95e3856f073d33fc76abad
ivn <ivanderseah08@gmail.com>
2025-09-24 11:07:36 +0700

    Update powerlifting_regression.m

--- powerlifting_regression.m
+++ powerlifting_regression.m
@@ -1,12 +1,6 @@
-clear; clc;
+clear; clc; 
 
 data = readtable('openpowerlifting-2024-01-06-4c732975.csv');
-
-vars = {'Sex','Equipment','Age','BodyweightKg','Country',...
-        'Best3SquatKg','Best3BenchKg','Best3DeadliftKg'};
-
+vars = {'Sex','Equipment','Age','BodyweightKg','Country', 'Best3SquatKg', 'Best3BenchKg','Best3DeadliftKg'};
 data = data(:, vars);
-
 data = rmmissing(data);
-
-disp(head(data));
